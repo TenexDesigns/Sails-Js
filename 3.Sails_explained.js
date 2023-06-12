cc20c456aac57968ae32130abd2cd76f08a325d7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-12 20:05:25 +0300

    Create 3.Sails_explained.js

---
+++ 3.Sails_explained.js
@@ -0,0 +1,65 @@
+Sails.js is a model-view-controller (MVC) web application framework built on top of the Node.js environment. It is designed to make it easy to build custom, enterprise-grade Node.js applications and APIs, resembling the MVC architecture from frameworks like Ruby on Rails. Sails.js is especially suitable for building real-time features like chat and data-driven web apps github.com en.wikipedia.org.
+
+The framework is built on top of Express.js, one of the most popular Node.js libraries, and Socket.io, a JavaScript library/engine for adding real-time, bidirectional, event-based communication to applications sitepoint.com.
+
+Sails.js comes with several features, such as:
+
+Blueprints: Auto-generating REST APIs for basic CRUD operations.
+Frontend agnostic: Compatibility with any frontend strategy, including Angular, React, Vue, and mobile app development.
+Ease of development: Convention-over-configuration approach, reducing boilerplate code and increasing productivity.
+Waterline ORM: An adapter-based ORM for Node.js, simplifying the data layer of your application.
+Pros:
+
+Simplifies building RESTful APIs, real-time applications, and data-driven web apps.
+Built-in support for WebSockets.
+Integration with popular frontend frameworks.
+Convention-over-configuration approach, reducing boilerplate code.
+Cons:
+
+Learning curve for beginners.
+May be overkill for small projects that do not require advanced features.
+Less active development compared to some other frameworks.
+Overall, Sails.js is a powerful framework for building Node.js applications, particularly those that require real-time features and data-driven functionality builtin.com.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
